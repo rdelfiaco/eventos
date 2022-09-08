25a96cc584a85bcdd89d5692321e042568525d1f
GabrielMeloBatista <gabrielmelobatista@hotmail.com>
2022-09-08 11:22:24 -0300

    adicionado formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +9,63 @@ import { Component } from '@angular/core';
 })
 
 
-export class AppComponent {
-  title = 'Detalhe do evento:' ;
+export class AppComponent implements OnInit  {
+  title = 'Detalhe do evento:';
+  evento: any;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  onSubmit = () => {
+
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.evento = params['evento'];
+    });
+  }
+
+  onRecordButton(): void {
+    alert("Reagendar");
+  }
+
+  onResqueduleButton(): void {
+    alert("Gravar");
+  }
+  onCancelButton(): void {
+    alert("Cancelar");
+  }
+  onHistoricButton(): void {
+    alert("Histórico");
+  }
+  onRelatedProductsButton(): void {
+    alert("Produtos");
+  }
+  onRelatedImageButton(): void {
+    alert("Imagem");
+  }
+  onCloseButton(): void {
+    alert("fechar");
+  }
+
+  eventos = new FormGroup({
+    setor: new FormControl('', [Validators.required]),
+    tipo: new FormControl(''),
+    motivo: new FormControl(''),
+    status: new FormControl(''),
+    prioridade: new FormControl(''),
+    resolverAte: new FormControl(''),
+    registradoPor: new FormControl(''),
+    respondidoEm: new FormControl(''),
+    destinatario: new FormControl(''),
+    cliente: new FormControl(''),
+    observacaoOrigem: new FormControl(''),
+  });
 }
 
-function loadEvento(){
+
+function loadEvento() {
 
 }
